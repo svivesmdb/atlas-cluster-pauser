b7287e3a9195069f03bdb2b7e7dea1391588ce95
Sergi Vives <sergi.vives@mongodb.com>
2021-06-09 16:35:02 +0200

    Pushing cluster pauser Realm app

---
+++ ClusterPauser/functions/resumeCluster/source.js
@@ -0,0 +1,27 @@
+exports = async function() {
+  
+  // Supply projectID and clusterNames...
+  const projectID = context.values.get("ProjectID");
+  const clusterNames = ['MyAtlasCluster'];
+
+  // Get stored credentials...
+  const username = context.values.get("AtlasPublicKey");
+  const password = context.values.get("AtlasPrivateKey");
+
+  // Set desired state...
+  const body = {paused: false};
+
+  var result = "";
+  
+  clusterNames.forEach(async function (name) {
+    result = await context.functions.execute('modifyCluster', username, password, projectID, name, body)
+    console.log("Cluster " + name + ": " + EJSON.stringify(result));
+    
+    if (result.error) { 
+      return result;
+    }
+  })
+
+
+  return clusterNames.length + " clusters resumed"; 
+};
